7b76a47ee0edf579606471b4c61bc0a12226e86c
Jyotika Patil <jyotikapatil130698@gmail.com>
2023-03-13 03:39:17 -0700

    Defined entity attributes

--- src/main/java/com/database/entity/Exam.java
+++ src/main/java/com/database/entity/Exam.java
@@ -1,4 +1,36 @@
 package com.database.entity;
 
 public class Exam {
+
+    private int examID;
+    int patientID;
+
+    public Exam(int examID, int patientID) {
+        this.examID = examID;
+        this.patientID = patientID;
+    }
+
+    public int getExamID() {
+        return examID;
+    }
+
+    public void setExamID(int examID) {
+        this.examID = examID;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    @Override
+    public String toString() {
+        return "Exam{" +
+                "examID=" + examID +
+                ", patientID=" + patientID +  //TODO
+                '}';
+    }
 }
--- src/main/java/com/database/entity/Patient.java
+++ src/main/java/com/database/entity/Patient.java
@@ -1,4 +1,49 @@
 package com.database.entity;
 
+import java.util.Set;
+
 public class Patient {
+
+    private int patientId;
+    private String name;
+    private Set<Integer> exams;
+
+    public Patient(int patientId, String name, Set<Integer> exams) {
+        this.patientId = patientId;
+        this.name = name;
+        this.exams = exams;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Integer> getExams() {
+        return exams;
+    }
+
+    public void setExams(Set<Integer> exams) {
+        this.exams = exams;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId=" + patientId +
+                ", name='" + name + '\'' +
+                ", exams=" + exams +
+                '}';
+    }
 }
